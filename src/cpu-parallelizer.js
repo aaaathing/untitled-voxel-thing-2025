19ef4508c66fd67756bc3100e11071ae51cd4017
aaaathing <aarontao950@gmail.com>
2025-02-02 17:00:33 -0800

    allow types on vars and correctly modify memory

--- src/cpu-parallelizer.js
+++ src/cpu-parallelizer.js
@@ -1,14 +1,20 @@
-import "https://cdn.jsdelivr.net/npm/acorn@8.14.0/dist/acorn.min.js"
+import * as parser from "https://cdn.skypack.dev/@babel/parser?min"
 import "https://cdn.jsdelivr.net/npm/acorn-walk@8.3.4/dist/walk.min.js"
+globalThis.parser = parser
 
-const workerURL = URL.createObjectURL(new Blob([`
+
+export class CPU_Parallelizer{
+	constructor(){
+		this.w = []
+		this.curId = 0
+		this.code = [`
 let funcs = new Map()
 onmessage = function(e){
 	const pkt = e.data
 	switch(pkt.type){
-		case "create":
-			funcs.set(pkt.name, new Function(pkt.func)())
-			break
+		//case "create":
+		//	funcs.set(pkt.name, new Function(pkt.func)())
+		//	break
 		case "run":
 			for(let x=pkt.minX; x<pkt.maxX; x++) for(let y=pkt.minY; y<pkt.maxY; y++) for(let z=pkt.minZ; z<pkt.maxZ; z++){
 				funcs.get(pkt.name)(x,y,z, ...pkt.args)
@@ -16,42 +22,49 @@ onmessage = function(e){
 			break
 	}
 }
-`], { type: "text/javascript" }))
-
-export class CPU_Parallelizer{
-	constructor(){
-		this.w = []
-		let wcount = (navigator.hardwareConcurrency||1)-1||1
-		for(let i=0;i<wcount;i++){
-			this.w.push(new Worker(workerURL))
-		}
-		this.curId = 0
+`]
+		this.classes = {}
 	}
-	create(include, func){
-		let oldStr = include.map(i => i.toString()) + "; return "+func.toString()
-		let ast = acorn.parse(oldStr, {ecmaVersion: 2025, allowReturnOutsideFunction:true})
-		getDefinitions(ast)
+	include(source){
+		let ast = parse(source, this)
 		let replace = []
 		acorn.walk.full(ast, function(node, state, type){
 			if(node.getFromBuffer){
-				replace.push([node, node.getFromBuffer[0]+"["+node.getFromBuffer[1]+"+"+node.getFromBuffer[2]+"]"])
+				let dataType
+				switch(node.getFromBuffer.type.typeAnnotation.typeAnnotation.type){
+					case "TSNumberKeyword": dataType = "Float32"; break
+					case "TSBooleanKeyword": dataType = "Uint8"; break
+				}
+				if(node.getFromBuffer.assign){ // convert a.b=1 to mem.set(a+0,1)
+					replace.push([node.getFromBuffer.assign, node.getFromBuffer.inWhichBuffer+".set"+dataType+"("+node.getFromBuffer.name+"+"+node.getFromBuffer.offset+","+source.substring(node.getFromBuffer.assign.right.start,node.getFromBuffer.assign.right.end)+")"])
+				}else{ // convert a.b to mem.get(a+0)
+					replace.push([node, node.getFromBuffer.inWhichBuffer+".get"+dataType+"("+node.getFromBuffer.name+"+"+node.getFromBuffer.offset+")"])
+				}
 			}
 			if(node.remove){
 				replace.push([node, ""])
 			}
+			if(node.typeAnnotation){
+				replace.push([node.typeAnnotation, ""])
+			}
 		})
+		
 		replace.sort((a,b) => (a[0].start-b[0].start)||0)
 		let str="",previ=0
 		for(let i of replace){
-			str+=oldStr.slice(previ,i[0].start)
+			str+=source.slice(previ,i[0].start)
 			str+=i[1]
 			previ=i[0].end
 		}
-		str+=oldStr.slice(previ)
+		str+=source.slice(previ)
 		console.log(str)
+		this.code.push(str)
+	}
+	create(func){
+		let name = "func"+this.curId++
+		this.include("funcs.set('"+name+"',"+func.toString()+");")
 
-		let name = this.curId++
-		for(let w of this.w) w.postMessage({type:"create", name})
+		//for(let w of this.w) w.postMessage({type:"create", name})
 		return function(sx,sy,sz,...args){
 			let step = this.w.length/sx
 			let x = 0
@@ -61,39 +74,68 @@ export class CPU_Parallelizer{
 			}
 		}
 	}
+	done(){
+		const workerURL = URL.createObjectURL(new Blob(this.code, { type: "text/javascript" }))
+		let wcount = (navigator.hardwareConcurrency||1)-1||1
+		for(let i=0;i<wcount;i++){
+			this.w.push(new Worker(workerURL))
+		}
+	}
+	createBuffer(){
+		return new DataView(new SharedArrayBuffer(0,{maxByteLength:2**34}))
+	}
 }
 
-export function getDefinitions(ast){
-	let vartypes = {}, classes = {}
+acorn.walk.base.ClassProperty = acorn.walk.base.PropertyDefinition
+export function parse(source, parallelizer){
+	let ast = parser.parse(source, {plugins:["typescript","estree"]}).program
+	let {classes} = parallelizer
+	let vartypes = {}
 	acorn.walk.ancestor(ast, {
-		CallExpression:function(node, state, ancestors){
-			// like: typeof thing === Number
-			if(node.callee.name === "type"){
-				let varname = node.arguments[0].name
-				let type = node.arguments[1].name
-				let inWhichBuffer = node.arguments[2].name
-				if(!classes[type]) throw new Error("missing class for "+type)
-				vartypes[varname] = {block:ancestors.findLast(n => n.type === "BlockStatement"), class:classes[type], inWhichBuffer}
-				node.remove = true
-			}
-		},
 		MemberExpression:function(node, state, ancestors){
 			if(vartypes[node.object.name] && ancestors.includes(vartypes[node.object.name].block)){
 				let type = vartypes[node.object.name]
 				let prop = type.class.properties[node.property.name]
 				if(!prop) throw new Error("missing property "+node.property.name)
-				node.getFromBuffer = [type.inWhichBuffer, node.object.name, prop.offset]
+				let pa = ancestors[ancestors.length-2]
+				node.getFromBuffer = {
+					inWhichBuffer:type.inWhichBuffer, name:node.object.name, offset:prop.offset, size:prop.size, type:prop.type,
+					assign: pa.type === "AssignmentExpression" && pa.left === node ? pa : null
+				}
 			}
 		},
 		ClassDeclaration:function(node, state, ancestors){
 			let properties = {}, offset = 0
 			for(let d of node.body.body){
-				if(d.type === "PropertyDefinition"){
-					properties[d.key.name] = {offset}
-					offset++ //todo: depend on type
+				if(d.type === "ClassProperty"){
+					let size = sizeOf(d)
+					properties[d.key.name] = {offset, size, type:d}
+					offset += size
 				}
 			}
 			classes[node.id.name] = {block:ancestors.findLast(n => n.type === "BlockStatement"), properties}
 		}
+	}, {
+		...acorn.walk.base,
+		FunctionDeclaration:function(node, state, c){
+			for(let d of node.params){
+				if(d.typeAnnotation && classes[d.typeAnnotation.typeAnnotation.typeName.name]){
+					let varname = d.name
+					let type = d.typeAnnotation.typeAnnotation.typeName.name
+					let inWhichBuffer = d.typeAnnotation.typeAnnotation.typeParameters.params[0].typeName.name
+					vartypes[varname] = {block:node.body, class:classes[type], inWhichBuffer}
+				}
+			}
+			acorn.walk.base.FunctionDeclaration(node,state,c)
+		},
+		FunctionExpression:function(node, state, c){ this.FunctionDeclaration(node,state,c) },
 	})
+	return ast
+}
+function sizeOf(node){
+	switch(node.typeAnnotation.typeAnnotation.type){
+		case "TSNumberKeyword": return 4
+		case "TSBooleanKeyword": return 1
+		default: throw new Error("no type "+node.typeAnnotation.typeAnnotation.type)
+	}
 }
--- src/world/world.js
+++ src/world/world.js
@@ -1,17 +1,15 @@
-class PObject{
-	x;y;z
-	bounds_min_x;bounds_min_y;bounds_min_z
-	bounds_max_x;bounds_max_y;bounds_max_z
-	vel_x;vel_y;vel_z
-	volume
-}
-
 export class World{
 	objects = []
 	constructor(parallelizer){
-		this.collide = parallelizer.create([PObject], function(x,y,z,allObjects,o1,o2){
-			type(o1, PObject, allObjects)
-			type(o2, PObject, allObjects)
+		parallelizer.include(`
+class PObject{
+	x:number; y:number; z:number
+	bounds_min_x:number;bounds_min_y:number;bounds_min_z:number
+	bounds_max_x:number;bounds_max_y:number;bounds_max_z:number
+	vel_x:number;vel_y:number;vel_z:number
+	volume:number
+}`)
+		this.collide = parallelizer.create(`function(x,y,z,allObjects,o1:PObject<allObjects>,o2:PObject<allObjects>){
 			let otherX = Math.floor(o1.x+o1.bounds_min_x+x - o2.x-o2.bounds_min_x)
 			let otherY = Math.floor(o1.y+o1.bounds_min_y+y - o2.y-o2.bounds_min_y)
 			let otherZ = Math.floor(o1.z+o1.bounds_min_z+z - o2.z-o2.bounds_min_z)
@@ -19,7 +17,9 @@ export class World{
 				o1.vel_x = 0, o1.vel_y = 0, o1.vel_z = 0
 				o2.vel_x = 0, o2.vel_y = 0, o2.vel_z = 0
 			}
-		})
+		}`)
+		
+		parallelizer.done()
 	}
 	async tick(){
 		for(let i=0;i<this.objects.length; i++)for(let j=0;j<this.objects.length; j++){
